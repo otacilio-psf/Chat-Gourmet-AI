0d7a36b3f6425f7775b1f7f551e6da541fdc7505
Otacilio Filho <otaciliopedro@gmail.com>
2024-10-02 17:40:34 +0200

    Ruff format

--- vector-db/load_data_pre_embedded.py
+++ vector-db/load_data_pre_embedded.py
@@ -7,8 +7,8 @@ import os
 QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
 QDRANT_API_KEY = os.getenv("QDRANT_API_KEY", None)
 
-def init():
 
+def init():
     client = QdrantClient(QDRANT_URL, api_key=QDRANT_API_KEY)
 
     collection_name = "recipes"
@@ -48,21 +48,35 @@ def batched(iterable, n):
 
 def load_data(client, collection_name):
     dataset = load_dataset(
-        "otacilio-psf/recipe_short_dense_and_sparse_embeddings", split="train", streaming=True
-    ).select_columns(["id", "title", "NER", "document", "all-MiniLM-L6-v2", "bm42-all-minilm-l6-v2-attentions"])
+        "otacilio-psf/recipe_short_dense_and_sparse_embeddings",
+        split="train",
+        streaming=True,
+    ).select_columns(
+        [
+            "id",
+            "title",
+            "NER",
+            "document",
+            "all-MiniLM-L6-v2",
+            "bm42-all-minilm-l6-v2-attentions",
+        ]
+    )
 
     batch_size = 100
 
-    for batch in tqdm(batched(dataset, batch_size), total=int(350000/batch_size), desc="Loading data"):
+    for batch in tqdm(
+        batched(dataset, batch_size),
+        total=int(350000 / batch_size),
+        desc="Loading data",
+    ):
         ids = [point.pop("id") for point in batch]
         vectors = [point.pop("all-MiniLM-L6-v2") for point in batch]
         sparse_vectors = []
         for point in batch:
             s_vector_dict = point.pop("bm42-all-minilm-l6-v2-attentions")
             s_vector = models.SparseVector(
-                        values=s_vector_dict["values"],
-                        indices=s_vector_dict["indices"]
-                    )
+                values=s_vector_dict["values"], indices=s_vector_dict["indices"]
+            )
             sparse_vectors.append(s_vector)
         for point in batch:
             point["ingredients_list"] = " ".join(point["NER"])
@@ -74,16 +88,14 @@ def load_data(client, collection_name):
                 ids=ids,
                 vectors={
                     "all-MiniLM-L6-v2": vectors,
-                    "bm42-all-minilm-l6-v2-attentions": sparse_vectors
+                    "bm42-all-minilm-l6-v2-attentions": sparse_vectors,
                 },
                 payloads=batch,
             ),
         )
 
 
-
 if __name__ == "__main__":
-
     client, collection_name = init()
 
     load_data(client, collection_name)
